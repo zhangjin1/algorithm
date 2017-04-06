cb7081e80c2dba25bfa1036751d537c7ab98c103
zhangjin <zhonggongzhangjin@163.com>
2017-04-06 18:35:53 +0800

    折半查找

---
+++ src/main/java/org/zj/SortTest.java
@@ -0,0 +1,14 @@
+package org.zj;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		// int[] elements = new int[] { 1, 0, 4, 34, 22, 13, 46, 55, 12, 9, 39,
+		// 24, 87, 33 };
+		int[] elements = new int[] { 0, 1 };
+		ZheBanChaRuSort.sort(elements);
+		for (int i = 0; i < elements.length; i++) {
+			System.out.print(elements[i] + ",");
+		}
+	}
+}
--- src/main/java/org/zj/ZheBanChaRuSort.java
+++ src/main/java/org/zj/ZheBanChaRuSort.java
@@ -2,4 +2,33 @@ package org.zj;
 
 public class ZheBanChaRuSort {
 
+	public static void sort(int[] elements) {
+		int size = elements.length;
+		int position, key;
+		for (int i = 1; i < size; i++) {
+			key = elements[i];
+			position = getPosition(0, i - 1, elements, key);
+			for (int j = i; j > position; j--) {
+				elements[j] = elements[j - 1];
+			}
+			elements[position] = key;
+		}
+	}
+
+	public static int getPosition(int low, int high, int[] elements, int key) {
+		if (low >= high) {
+			if (key > elements[low]) {
+				return low + 1;
+			} else {
+				return low;
+			}
+		}
+		int mid = (low + high) / 2;
+		if (key > elements[mid]) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+		return getPosition(low, high, elements, key);
+	}
 }
